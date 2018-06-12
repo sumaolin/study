c84e7b587573b48c6a828b5a52f0fea086ad0b8a
sumaolin <sumaolin@gmail.com>
2018-06-12 21:16:17 +0800

    feat(3 router-request & koa-bodyparser):

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const Koa = require('koa')
 // 后面加()
 const Router = require('koa-router')
+const bodyParser = require('koa-bodyparser')
 const app = new Koa()
 
+app.use(bodyParser())
 const irouter = new Router({
-  prefix: '/users'
+  prefix: '/user'
 })
+
 irouter.get('/', async (ctx, next) => {
-  ctx.response.body = '<h1>index page</h1>'
-  await next()
+  ctx.response.body = `
+  <form action="/user/register" method="post">
+    <input name="name" type="text" placeholder="请输入用户名：ikcamp"/>
+    <br/>
+    <input name="password" type="text" placeholder="请输入密码：123456"/>
+    <br/>
+    <button>GoGoGo</button>
+  </form>
+  `
 })
 
 irouter.get('/:id', async (ctx, next) => {
@@ -16,8 +26,13 @@ irouter.get('/:id', async (ctx, next) => {
   await next()
 })
 
-irouter.get('/home', async (ctx, next) => {
-  ctx.response.body = '<h1>home page</h1>'
+irouter.post('/register', async (ctx, next) => {
+  let { name, password } = ctx.request.body
+  if (name == 'sumaolin' && password == '123456') {
+    ctx.response.body = `hello ${name}`
+  } else {
+    ctx.response.body = '<h1>password error</h1>'
+  }
   await next()
 })
 
@@ -28,6 +43,8 @@ irouter.get('/404', async (ctx, next) => {
 
 irouter.all('/*', async (ctx, next) => {
   console.log('all action')
+  console.log(ctx.request.query)
+  console.log(ctx.request.querystring)
   await next()
 })
 
