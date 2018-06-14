d449594654ed463728831150cafb3d5417ba3bb2
sumaolin <sumaolin@gmail.com>
2018-06-14 21:49:47 +0800

    feat(5 Nunjucks 模版引擎):

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const Koa = require('koa')
-
+const path = require('path')
+const nunjucks = require('koa-nunjucks-2')
 const bodyParser = require('koa-bodyparser')
 const app = new Koa()
 const router = require('./router')
+
+console.log(path.join(__dirname, 'views'))
+app.use(
+  nunjucks({
+    ext: 'html',
+    path: path.join(__dirname, 'views'),
+    nunjucksConfig: {
+      trimBlocks: true
+    }
+  })
+)
 
 app.use(bodyParser())
 router(app)
--- controller/user.js
+++ controller/user.js
@@ -1,15 +1,9 @@
 const userService = require('../service/user')
 module.exports = {
   login: async (ctx, next) => {
-    ctx.response.body = `
-    <form action="/user/register" method="post">
-      <input name="name" type="text" placeholder="请输入用户名：ikcamp"/>
-      <br/>
-      <input name="password" type="text" placeholder="请输入密码：123456"/>
-      <br/>
-      <button>GoGoGo</button>
-    </form>
-    `
+    await ctx.render('user/login', {
+      btnName: 'login'
+    })
   },
   postsParams: async (ctx, next) => {
     ctx.response.body = 'user Id:' + ctx.params.id
