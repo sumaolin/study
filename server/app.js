a34ff7fbd7b9af5c6cfd8da552cfb6c0d922e802
sumaolin <sumaolin619@gmail.com>
2018-09-21 10:57:06 +0800

    refactor(all middleware move to /middleware):
    disable koa-babyparser middleware

--- server/app.js
+++ server/app.js
@@ -1,23 +1,10 @@
-const path = require('path')
 const Koa = require('koa')
-const views = require('koa-view')
-const Static = require('koa-static')
-// const bodyParse = require('koa-bodyparser')
 // customer
 const middlewares = require('./middleware')
 const routers = require('./routers')
 const app = new Koa()
 
 middlewares(app)
-app.use(Static(path.resolve(__dirname, './public')))
-
-app.use(
-  views(path.resolve(__dirname, './views'), {
-    extension: 'ejs'
-  })
-)
-
-// app.use(bodyParse())
 
 app.use(routers.routes()).use(routers.allowedMethods())
 
--- server/middleware/index.js
+++ server/middleware/index.js
@@ -1,5 +1,17 @@
+const path = require('path')
+const views = require('koa-view')
+const Static = require('koa-static')
+// const bodyParse = require('koa-bodyparser')
 const miSend = require('./mi-send')
 
 module.exports = app => {
+  app.use(Static(path.resolve(__dirname, './../public')))
+  // app.use(bodyParse())
+
+  app.use(
+    views(path.resolve(__dirname, './../views'), {
+      extension: 'ejs'
+    })
+  )
   app.use(miSend())
 }
