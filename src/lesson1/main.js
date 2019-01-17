06965c9a2054954cbddcf3668894fdf02161e534
sumaolin <sumaolin619@gmail.com>
2019-01-17 17:42:04 +0800

    fixed: tsc main.ts -> main.js

---
+++ src/lesson1/main.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var baby_1 = require("./baby");
+var bb = new baby_1.Baby('Kevin');
+console.log(bb.getBabyName());
+console.log(baby_1.baby.getBabyName());
