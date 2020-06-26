adde591e500bd863df721228f80a059d0cd3c9dd
sumaolin <sumaolin619@gmail.com>
2020-06-26 15:26:13 +0800

    fix(logger middleware): 输出request body 中的参数值

--- nest-app/src/main.ts
+++ nest-app/src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
 import { LoggerMiddleware } from './middleware/logger.middleware';
 
 const log = new LoggerMiddleware();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
   app.use(log.use);
   await app.listen(3000);
 }
--- nest-app/src/middleware/logger.middleware.ts
+++ nest-app/src/middleware/logger.middleware.ts
@@ -8,7 +8,20 @@ export class LoggerMiddleware implements NestMiddleware {
     const code = req.statusCode;
     next();
 
-    const logFormat = `Method: ${req.method} \n Request original url ${req.originalUrl} \n IP: ${req.ip} \n Status code : ${code} \n`;
+    // const logFormat2 = `Method: ${req.method} \n Request original url ${req.originalUrl} \n IP: ${req.ip} \n Status code : ${code} \n`;
+
+    // 组装日志信息
+    const logFormat = `\n  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+      Request original url: ${req.originalUrl}
+      Method: ${req.method}
+      IP: ${req.ip}
+      Status code: ${code}
+      Parmas: ${JSON.stringify(req.params)}
+      Query: ${JSON.stringify(req.query)}
+      Body: ${JSON.stringify(
+        req.body,
+      )} \n  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    `;
 
     if (code >= 500) {
       Logger.error(logFormat);
